3f3eab23ed570292e308599b9e4698b5a6892d0d
Vik Paruchuri <vik.paruchuri@gmail.com>
2025-09-17 13:28:52 -0400

    Add mode option

--- datalab_sdk/settings.py
+++ datalab_sdk/settings.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     # Paths
     BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     LOGLEVEL: str = "DEBUG"
-    VERSION: str = "0.1.3"
+    VERSION: str = "0.1.6"
 
     # Base settings
     DATALAB_API_KEY: str | None = None
